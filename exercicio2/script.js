6da2cf28f37d8b361139b001560806fc3c955112
Luiz Dirceu <juninholdmarques@gmail.com>
2022-08-16 20:06:18 -0300

    atividade

---
+++ exercicio2/script.js
@@ -0,0 +1,16 @@
+let num = +prompt("insira um numero")
+
+for(let i = 1; i <= 10; i++){
+let tabuada = num * i
+console.log(`${num} x ${i} = ${tabuada}`) }
+    
+
+//     console.log(i)
+
+//     let num = prompt("Digite um numero: ")
+
+// for(let i=1; i<=10; i++){
+
+//     let tabuada = num * i
+    
+//     console.log(`${num} x ${i} = ${tabuada}`)
